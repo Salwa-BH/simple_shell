f46d9b04f1780cfd728449244127b24294955d89
Salwa-BH <batahsalwa@gmail.com>
2023-05-26 11:56:21 +0100

    modify

--- main.c
+++ main.c
@@ -5,50 +5,17 @@
 #include <stddef.h>
 
 /**
- * main - Simple shell program entry point
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
- * Return: Always 0
- */
-int main(void)
+ * main - func with infinite loop
+ * @ac: No use
+ * @av: No use
+ * Return: loop.
+ **/
+int main(int ac, char **av)
 {
-	char *buffer = NULL;
-	ssize_t read_bytes;
-	size_t buffer_size = BUFFER_SIZE;
-	char **args = NULL;
+	(void)av;
+	(void)ac;
 
-	while (1)
-	{
-		display_prompt();
-		read_bytes = getline(&buffer, &buffer_size, stdin);
-		if (buffer == NULL)
-		{
-			printf("\n");
-			break;
-		}
-		buffer[read_bytes - 1] = '\0'; /* Remove the newline character */
-
-		args = parse_command(buffer);
-
-		if (args == NULL)
-		{
-			free(buffer);
-			continue;
-		}
-
-		if (is_builtin_command(args[0]))
-		{
-			handle_builtin_command(args);
-		}
-		else
-		{
-			execute_command(args);
-		}
-
-		free_args(args);
-	}
-
-	free(buffer); /* Free the allocated buffer */
-
+	signal(SIGINT, controlC);
+	prompt();
 	return (0);
 }
